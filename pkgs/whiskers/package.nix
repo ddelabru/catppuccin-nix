de6ac4e641eb52c5bd44407d56ab8284cd73d472
Dominic Delabruere <dominic.delabruere@gmail.com>
2026-03-18 09:38:00 -0400

    Alias whiskers to nixpkgs catpuccin-whiskers

--- pkgs/whiskers/package.nix
+++ pkgs/whiskers/package.nix
@@ -1,29 +1,6 @@
 {
-  lib,
-  fetchCatppuccinPort,
-  nix-update-script,
-  rustPlatform,
+  catppuccin-whiskers,
 }:
 
-rustPlatform.buildRustPackage rec {
-  pname = "whiskers";
-  version = "2.5.1";
+catppuccin-whiskers
 
-  src = fetchCatppuccinPort {
-    port = "whiskers";
-    rev = "refs/tags/v${version}";
-    hash = "sha256-OLEXy9MCrPQu1KWICsYhe/ayVqxkYIFwyJoJhgiNDz4=";
-  };
-
-  cargoHash = "sha256-CVg7kcOTRa8KfDwiJHQhTPQfK6g3jOMa4h/BCUo3ehw=";
-
-  passthru = {
-    updateScript = nix-update-script { };
-  };
-
-  meta = {
-    description = "Soothing port creation tool for the high-spirited!";
-    homepage = "https://catppuccin.com";
-    license = lib.licenses.mit;
-  };
-}
