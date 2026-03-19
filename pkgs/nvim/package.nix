24b0791770c3a48014edf0b37fee3bb65e8c3ef7
Dominic Delabruere <dominic.delabruere@gmail.com>
2026-03-19 17:14:54 -0400

    Revert nvim alias

--- pkgs/nvim/package.nix
+++ pkgs/nvim/package.nix
@@ -1,5 +1,28 @@
 {
-  vimPlugins
+  lib,
+  vimUtils,
+  sources,
 }:
 
-vimPlugins.catppuccin-vim
+let
+  portName = "nvim";
+in
+
+vimUtils.buildVimPlugin rec {
+  pname = "catppuccin-nvim";
+  version = builtins.substring 0 7 src.rev;
+
+  src = sources.${portName};
+
+  nvimSkipModule = [
+    "catppuccin.groups.integrations.noice"
+    "catppuccin.groups.integrations.feline"
+    "catppuccin.lib.vim.init"
+  ];
+
+  meta = {
+    description = "Soothing pastel theme for ${portName}";
+    homepage = "https://github.com/catppuccin/${portName}";
+    license = lib.licenses.mit;
+  };
+}
